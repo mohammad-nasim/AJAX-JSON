fea095ff8480d9f1dfc264050f4b514b6dcbe8cb
nasim <mohammadnasim0175@gmail.com>
2021-07-07 10:02:42 +0600

    Displayed Json data on html using Ajax

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-let url = `https://jsonplaceholder.typicode.com/users`
+const btn = document.getElementById('btn')
+const URL = `https://jsonplaceholder.typicode.com/users`
 
-let ourRequest = new XMLHttpRequest();
+let user  = document.getElementById('user-info')
 
-ourRequest.open('GET', `https://jsonplaceholder.typicode.com/users`)
+btn.addEventListener('click' , function(e){
+   let ourRequest = new XMLHttpRequest();
 
-ourRequest.onload = function(){
-   let ourData = JSON.parse(ourRequest.responseText)
-   console.log(ourData[2].username)
+   ourRequest.open('GET', URL)
+
+   ourRequest.onload = function(){
+      let ourData = JSON.parse(ourRequest.responseText)
+      console.log(ourData)
+
+      renderHTML(ourData)
+   }
+
+   
+   ourRequest.send()
+})
+
+//function
+function renderHTML(data){
+
+   let recivedData = " "
+
+   for(i = 0; i < data.length ; i++){
+      recivedData += "<p>" + 
+                        "User Name is : " + "<strong>" + data[i].name + "</strong>" + " <br>" +
+                        "Email is : "+ "<strong>" + data[i].email + "</strong>"                 
+                     + " </p>" 
+   }
+
+   user.insertAdjacentHTML('beforeend' , recivedData)
+
+   
 }
-
-ourRequest.send()
